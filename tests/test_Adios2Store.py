6434153c7b7bb7df35814ca8af8f1eb4055ab225
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-01-10 21:38:22 -0500

    prepare for a test that writes using Adios2Store

--- tests/test_Adios2Store.py
+++ tests/test_Adios2Store.py
@@ -2,8 +2,25 @@ from __future__ import annotations
 
 import adios2py
 import numpy as np
+import pytest
 
 from xarray_adios2 import Adios2Store
+
+
+@pytest.fixture
+def test_file_store(tmp_path, sample_dataset):
+    ds = sample_dataset
+    filename = tmp_path / "test.bp"
+    with adios2py.File(filename, mode="w") as file:  # noqa: SIM117
+        with file.steps.next() as step:
+            for name, var in ds.variables.items():
+                step[name] = var
+                step[name].attrs["dimensions"] = " ".join(var.dims)
+                step[name].attrs["dtype"] = str(var.dtype)
+                for attr_name, attr in var.attrs.items():
+                    step[name].attrs[attr_name] = attr
+
+    return filename
 
 
 def test_ctor(test_file):
@@ -17,8 +34,10 @@ def test_open(test_file):
     assert store.ds.keys() == {"arr1d", "x", "time"}
 
 
-def test_load(test_file, sample_dataset):
-    with adios2py.File(test_file, mode="rra") as file:
+@pytest.mark.parametrize("filename", ["test_file", "test_file_store"])
+def test_load(filename, sample_dataset, request):
+    filename = request.getfixturevalue(filename)
+    with adios2py.File(filename, mode="rra") as file:
         with file.steps.next() as step:
             store = Adios2Store(step)
             vars, attrs = store.load()  # type: ignore[no-untyped-call]
